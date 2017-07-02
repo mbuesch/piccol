6a19981cde7a430084b674ba0d4d7a2623ed3c8e
Michael Buesch <m@bues.ch>
2017-07-02 17:30:09 +0200

    Add release script
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ makerelease.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+srcdir="$(dirname "$0")"
+[ "$(echo "$srcdir" | cut -c1)" = '/' ] || srcdir="$PWD/$srcdir"
+
+die() { echo "$*"; exit 1; }
+
+# Import the makerelease.lib
+# http://bues.ch/gitweb?p=misc.git;a=blob_plain;f=makerelease.lib;hb=HEAD
+for path in $(echo "$PATH" | tr ':' ' '); do
+	[ -f "$MAKERELEASE_LIB" ] && break
+	MAKERELEASE_LIB="$path/makerelease.lib"
+done
+[ -f "$MAKERELEASE_LIB" ] && . "$MAKERELEASE_LIB" || die "makerelease.lib not found."
+
+hook_get_version()
+{
+	version="$(cat piccol | grep -e PICCOL_VERSION | head -n1 | cut -d'"' -f2)"
+}
+
+project=piccol
+default_archives=py-sdist-bz2
+makerelease "$@"
