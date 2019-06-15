945910d68a27cecddcb366d55190548442485d83
Michael Buesch <m@bues.ch>
2019-06-15 23:20:35 +0200

    Update win scripts
    Signed-off-by: Michael Buesch <m@bues.ch>

--- win-standalone-build.cmd
+++ win-standalone-build.cmd
@@ -1,8 +1,26 @@
 @echo off
+rem
+rem Windows frozen package build script
+rem
+rem Copyright 2012-2019 Michael Buesch <m@bues.ch>
+rem
+rem This program is free software; you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation; either version 2 of the License, or
+rem (at your option) any later version.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License along
+rem with this program; if not, write to the Free Software Foundation, Inc.,
+rem 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+rem
 setlocal ENABLEDELAYEDEXPANSION
 
 set project=piccol
-
 
 set PATH=%PATH%;C:\WINDOWS;C:\WINDOWS\SYSTEM32
 for /D %%f in ( "C:\PYTHON*" ) do set PATH=!PATH!;%%f
@@ -10,16 +28,17 @@ for /D %%f in ( "%USERPROFILE%\AppData\Local\Programs\Python\Python*" ) do set P
 set PATH=%PATH%;%ProgramFiles%\7-Zip
 
 
-py -c "import re; print(re.match(r'.*^\s*PICCOL_VERSION\s*=\s*\"([\w\d\.\-_]+)\"\s*$.*', open('piccol').read(), re.DOTALL | re.MULTILINE).group(1))" > version.txt
-if ERRORLEVEL 1 goto error_version
-set /p version= < version.txt
-del version.txt
-
-
-set distdir=%project%-win64-standalone-%version%
-set sfxfile=%project%-win64-%version%.package.exe
+call :detect_version
+if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+	set winprefix=win32
+) else (
+	set winprefix=win64
+)
+set distdir=%project%-%winprefix%-standalone-%version%
+set sfxfile=%project%-%winprefix%-%version%.package.exe
 set bindirname=%project%-bin
 set bindir=%distdir%\%bindirname%
+set builddir=%bindir%\build
 set licensedirname=licenses
 set licensedir=%distdir%\%licensedirname%
 
@@ -27,89 +46,11 @@ set licensedir=%distdir%\%licensedirname%
 echo Building standalone Windows executable for %project% v%version%...
 echo.
 
-echo Please select GUI framework:
-echo   1)  Build with PyQt5 (default)
-echo   2)  Build with PySide4
-set /p framework=Selection: 
-if "%framework%" == "" goto framework_pyqt5
-if "%framework%" == "1" goto framework_pyqt5
-if "%framework%" == "2" goto framework_pyside4
-echo "Error: Invalid selection"
-goto error
-
-:framework_pyqt5
-echo Using PyQt5
-set excludes=PySide
-goto select_freezer
-
-:framework_pyside4
-echo Using PySide4
-set excludes=PyQt5
-goto select_freezer
-
-
-:select_freezer
-echo.
-echo Please select freezer:
-echo   1)  Build 'cx_Freeze' based distribution (default)
-echo   2)  Build 'py2exe' based distribution
-set /p buildtype=Selection: 
-if "%buildtype%" == "" goto build_cxfreeze
-if "%buildtype%" == "1" goto build_cxfreeze
-if "%buildtype%" == "2" goto build_py2exe
-echo "Error: Invalid selection"
-goto error
-
-
-:build_cxfreeze
-set buildtype=1
-echo === Creating the cx_Freeze distribution
 call :prepare_env
-py setup.py build_exe ^
-	--build-exe=%bindir% ^
-	--excludes=%excludes% ^
-	--silent
-if ERRORLEVEL 1 goto error_exe
-goto copy_files
-
-
-:build_py2exe
-set buildtype=2
-echo === Creating the py2exe distribution
-call :prepare_env
-py setup.py py2exe ^
-	--dist-dir=%bindir% ^
-	--bundle-files=3 ^
-	--ignores=win32api,win32con,readline ^
-	--excludes=%excludes% ^
-	--quiet
-if ERRORLEVEL 1 goto error_exe
-goto copy_files
-
-
-:copy_files
-echo === Copying additional files
-mkdir %licensedir%
-if ERRORLEVEL 1 goto error_copy
-copy README.* %distdir%\
-if ERRORLEVEL 1 goto error_copy
-copy foreign-licenses\*.txt %licensedir%\
-if ERRORLEVEL 1 goto error_copy
-copy COPYING %licensedir%\%project%-LICENSE.txt
-if ERRORLEVEL 1 goto error_copy
-
-
-echo === Generating startup wrapper
-set wrapper=%distdir%\%project%.cmd
-echo @set PATH=%bindirname%;%%PATH%%> %wrapper%
-echo @start %project%-bin\%project%.exe %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9>> %wrapper%
-if ERRORLEVEL 1 goto error_wrapper
-
-
-echo === Creating the distribution archive
-7z a -mx=9 -sfx7z.sfx %sfxfile% %distdir%
-if ERRORLEVEL 1 goto error_7z
-
+call :build_cxfreeze
+call :copy_files
+call :gen_startup_wrapper
+call :make_archive
 
 echo ---
 echo finished
@@ -117,18 +58,70 @@ pause
 exit /B 0
 
 
-:prepare_env
-echo === Preparing distribution environment
-rd /S /Q build 2>NUL
-rd /S /Q %distdir% 2>NUL
-del %sfxfile% 2>NUL
-timeout /T 2 /NOBREAK >NUL
-mkdir %distdir%
-if ERRORLEVEL 1 goto error_prep
-mkdir %bindir%
-if ERRORLEVEL 1 goto error_prep
-exit /B 0
+:detect_version
+	py -c "import re; print(re.match(r'.*^\s*PICCOL_VERSION\s*=\s*\"([\w\d\.\-_]+)\"\s*$.*', open('piccol').read(), re.DOTALL | re.MULTILINE).group(1))" > version.txt
+	if ERRORLEVEL 1 goto error_version
+	set /p version= < version.txt
+	del version.txt
+	exit /B 0
 
+
+:prepare_env
+	echo === Preparing distribution environment
+	rd /S /Q build 2>NUL
+	rd /S /Q %distdir% 2>NUL
+	del %sfxfile% 2>NUL
+	timeout /T 2 /NOBREAK >NUL
+	mkdir %distdir%
+	if ERRORLEVEL 1 goto error_prep
+	mkdir %bindir%
+	if ERRORLEVEL 1 goto error_prep
+	exit /B 0
+
+
+:build_cxfreeze
+	echo === Creating the cx_Freeze distribution
+	py setup.py ^
+		build ^
+		--build-base=%builddir% ^
+		build_exe ^
+		--build-exe=%bindir%
+	if ERRORLEVEL 1 goto error_exe
+	exit /B 0
+
+
+:copy_files
+	echo === Copying additional files
+	mkdir %licensedir%
+	if ERRORLEVEL 1 goto error_copy
+	copy README.* %distdir%\
+	if ERRORLEVEL 1 goto error_copy
+	copy foreign-licenses\*.txt %licensedir%\
+	if ERRORLEVEL 1 goto error_copy
+	copy COPYING %licensedir%\%project%-LICENSE.txt
+	if ERRORLEVEL 1 goto error_copy
+	exit /B 0
+
+
+:gen_startup_wrapper
+	echo === Generating startup wrapper
+	set wrapper=%distdir%\%project%.cmd
+	echo @set PATH=%bindirname%;%bindirname%\lib;%bindirname%\platforms;%bindirname%\imageformats;%%PATH%% > %wrapper%
+	echo @start %project%-bin\%project%.exe %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9 >> %wrapper%
+	if ERRORLEVEL 1 goto error_wrapper
+	exit /B 0
+
+
+:make_archive
+	echo === Creating the distribution archive
+	7z a -mx=9 -sfx7z.sfx %sfxfile% %distdir%
+	if ERRORLEVEL 1 goto error_7z
+	exit /B 0
+
+
+:error_basedir
+echo FAILED to CD to base directory
+goto error
 
 :error_version
 echo FAILED to detect version
